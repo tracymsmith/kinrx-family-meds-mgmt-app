63bf6f016913014d04fce8efe3d936a592259b0d
Tracy Smith <tmhsllc@icloud.com>
2024-10-28 16:08:06 -0500

    inserted data into postico tables to test and added same code in database.sql file in vscode to backup; need to debug error inserting data into postico joint patients_medicines table

--- database.sql
+++ database.sql
@@ -27,6 +27,10 @@ CREATE TABLE "patients_medicines" (
     "amount" VARCHAR(1000) NOT NULL,
     "frequency" VARCHAR(1000) NOT NULL
 );
-INSERT INTO "patients_medicines" (patient_id, medicine_id, dose, amount, frequency)
-VALUES (1, 2, '500 mg', '1 tablet', 'Twice daily');
+INSERT INTO "medicines" (medicine, dose, user_id)
+VALUES ('vitamin D3', '2000 IU / 50 mcg', 9),('acetaminophen', '650 mg', 9),('guanfacine', '1 mg', 9),('carvedilol', '25 mg', 9),('allopurinol', '100 mg', 9),('lisinopril', '20 mg', 9),('magnesium oxide', '250 mg', 9),('magnesium glicynate', '100 mg', 9),('levothyroxine', '112 mcg', 9),('aspirin', '81 mg', 9),('hydralazine', '10 mg', 9),('albuterol', '90 mcg', 9),('warfarin', '5 mg', 9),('rosuvastatin', '10 mg', 9),('jardiance', '10 mg', 9),('trazadone', '50 mg', 9),('metformin', '1,000 mg', 9),('furosemide', '20 mg', 9);
+INSERT INTO "patients" (patient, date_of_birth, user_id)
+VALUES ('Ward', '06/18/48', 9),('June', '06/27/39', 9),('Theodore', '04/07/07', 9),('Wally', '09/24/59', 9);
+-- INSERT INTO "patients_medicines" (patient_id, medicine_id, dose, amount, frequency)
+-- VALUES (1, 2, '500 mg', '1 tablet', 'Twice daily');
 
