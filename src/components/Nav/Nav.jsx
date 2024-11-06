03d7b08645bc9f51389252d5bf15faf57782a7c0
Tracy Smith <tmhsllc@icloud.com>
2024-11-05 21:04:53 -0600

    fixed nav routes for home, user, patient pages

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -24,12 +24,12 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
+            <Link className="navLink" to="/home">
               Home
             </Link>
 
-            <Link className="navLink" to="/info">
-              Info Page
+            <Link className="navLink" to="/patient">
+              Patient Page
             </Link>
 
             <LogOutButton className="navLink" />
